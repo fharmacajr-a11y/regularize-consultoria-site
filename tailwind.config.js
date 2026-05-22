9e513e9cde3617a2e2bb6aa95c64c4469ac80c67
Junior <fharmacajr@gmail.com>
2026-05-22 17:03:17 -0300

    Performance: substitui Tailwind CDN por CSS local

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "brand-dark": "#1a3a5c",
+        "brand": "#1565c0",
+        "brand-light": "#29b6f6"
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+}
